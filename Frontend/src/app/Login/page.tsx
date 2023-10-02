a1d4fb55b4ddce5abd3ed91718bd7679004f1449
daniellaiex <88719093+daniellaiex@users.noreply.github.com>
2023-10-02 13:24:15 +0800

    Update login/page.tsx typescript error

--- Frontend/src/app/Login/page.tsx
+++ Frontend/src/app/Login/page.tsx
@@ -94,7 +94,7 @@ const Login:React.FC = () => {
               <button
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                onClick={handleSignIn}
+                onClick={() => handleSignIn}
               >
                 Sign in
               </button>
