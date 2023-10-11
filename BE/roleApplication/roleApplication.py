02def9512e9872e157d670aca2069d9ff49a4e15
Nicholas Goh <nicholasgbr99@gmail.com>
2023-10-11 15:22:06 +0800

    update to role applicants

--- BE/roleApplication/roleApplication.py
+++ BE/roleApplication/roleApplication.py
@@ -24,6 +24,7 @@ class RoleApplication(db.Model):
     role_listing_id = db.Column(db.Integer, nullable=False)
     staff_id = db.Column(db.Integer, nullable=False)
     role_app_status = db.Column(db.Enum(AppliedEnum), nullable=False)
+    role_app_ts_create = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
 
     def __init__(self, role_listing_id, staff_id, role_app_status):
         self.role_listing_id = role_listing_id
@@ -35,9 +36,9 @@ class RoleApplication(db.Model):
             "role_app_id": self.role_app_id,
             "role_listing_id": self.role_listing_id,
             "staff_id": self.staff_id,
-            "role_app_status": self.role_app_status.name
+            "role_app_status": self.role_app_status.name,
+            "role_app_ts_create": self.role_app_ts_create
         }
-    
 # Create a new RoleApplication
 @app.route("/createRoleApplication", methods=["POST"])
 def create_role_application():
